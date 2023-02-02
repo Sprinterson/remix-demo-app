2864a2c3fd78f284e525d0b29712c726a7ce07be
Charles Raviaud <charles.raviaud@protonmail.com>
2023-02-02 15:17:46 +0100

    feat: create components, styles and notes route

--- app/root.jsx
+++ app/root.jsx
@@ -5,22 +5,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
+
+import MainNavigation from '~/components/MainNavigation';
+import styles from '~/styles/main.css';
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
@@ -28,4 +34,8 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
 }
