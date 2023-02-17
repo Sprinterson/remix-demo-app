33110cbbb27046059b00b924943630796afe6e02
Charles Raviaud <charles.raviaud@protonmail.com>
2023-02-17 11:34:20 +0100

    feat: create dynamic routes

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,5 @@
-import { redirect } from '@remix-run/node';
-import { Link, useLoaderData } from '@remix-run/react';
+import { json, redirect } from '@remix-run/node';
+import { Link, useCatch, useLoaderData } from '@remix-run/react';
 
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
 import NoteList, { links as noteListLinks } from '~/components/NoteList';
@@ -18,6 +18,17 @@ export default function NotesPage() {
 
 export async function loader() {
   const notes = await getStoredNotes();
+
+  if (!notes || notes.length === 0) {
+    throw json(
+      { message: 'Could not find any notes.' },
+      {
+        status: 404,
+        statusText: 'Not Found',
+      }
+    );
+  }
+
   return notes;
 }
 
@@ -38,6 +49,19 @@ export async function action({ request }) {
 
 export function links() {
   return [...newNoteLinks(), ...noteListLinks()];
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+
+  const message = caughtResponse.data?.message || 'Data not found.';
+
+  return (
+    <main>
+      <NewNote />
+      <p className='info-message'>{message}</p>
+    </main>
+  );
 }
 
 export function ErrorBoundary({ error }) {
